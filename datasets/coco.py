da1c5a6b874f599357c39f9d1f4825567d3b424d
TrueWarg <kl@appkode.ru>
2020-07-08 12:03:22 +0200

    [dataset] Enable using custom coco-like dataset

--- datasets/coco.py
+++ datasets/coco.py
@@ -156,3 +156,17 @@ def build(image_set, args):
     img_folder, ann_file = PATHS[image_set]
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
     return dataset
+
+
+def build_custom(image_set, args):
+    root = Path(args.coco_path)
+    assert root.exists(), f'provided COCO path {root} does not exist'
+    mode = 'instances'
+    PATHS = {
+        "train": (root / "train", root / "annotations" / f'{mode}_train.json'),
+        "val": (root / "val", root / "annotations" / f'{mode}_val.json'),
+    }
+
+    img_folder, ann_file = PATHS[image_set]
+    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    return dataset
--- models/detr.py
+++ models/detr.py
@@ -305,6 +305,8 @@ def build(args):
     num_classes = 20 if args.dataset_file != 'coco' else 91
     if args.dataset_file == "coco_panoptic":
         num_classes = 250
+    if args.dataset_file == 'custom':
+        num_classes = args.dataset_num_classes
     device = torch.device(args.device)
 
     backbone = build_backbone(args)
